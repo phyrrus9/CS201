4ff74e5922ecbeb16fa973d27fd4003212ac9618
Ethan Laur <phyrrus9@gmail.com>
2014-04-10 22:29:26 -0400

    Seemingly finished Prject9

--- Project9/Project9/main.cpp
+++ Project9/Project9/main.cpp
@@ -12,8 +12,10 @@
 #include <string.h>
 #include <ctype.h>
 
-int tablesize;
+int tablesize, listsize;
 static char table[2048][32];
+static int linearprobeavg, quadraticprobeavg;
+static int twc, tuc;
 
 unsigned int hash(char *w)
 {
@@ -27,17 +29,26 @@ unsigned int hash(char *w)
 	return temp % tablesize;
 }
 
-unsigned int linearprobe(unsigned int hash)
+unsigned int linearprobe(unsigned int hash, unsigned int *comparisons = NULL, char *sv = NULL)
 {
-	while (table[hash][0] != 0) //empty slot
+	while (1) //empty slot
 	{
+		if (comparisons == NULL)
+		{
+			if (table[hash][0] == 0) return hash;
+		}
+		else
+		{
+			if (strcmp(table[hash], sv) == 0) return hash;
+			++(*comparisons);
+		}
 		if (hash > tablesize) hash = 0;
 		else hash++;
 	}
-	return hash;
+	return hash; //never gets here
 }
 
-unsigned int quadraticprobe(unsigned int hash)
+unsigned int quadraticprobe(unsigned int hash, unsigned int *comparisons = NULL, char *sv = NULL)
 {
 	unsigned int i = 1, a = hash, b = a;
 	if (!table[hash][0]) return hash;
@@ -47,8 +58,18 @@ unsigned int quadraticprobe(unsigned int hash)
 		b = hash - (i * i);
 		while (b > tablesize) a -= tablesize;
 		while (b > tablesize) b -= tablesize;
-		if (!table[a][0]) return a;
-		if (!table[b][0]) return b;
+		if (comparisons == NULL)
+		{
+			if (!table[a][0]) return a;
+			if (!table[b][0]) return b;
+		}
+		else
+		{
+			if (strcmp(table[hash], sv) == 0) return hash;
+			if (strcmp(table[a], sv) == 0) return a;
+			if (strcmp(table[b], sv) == 0) return b;
+			*comparisons += 2;
+		}
 		++i;
 	}
 	return hash; //never gets here
@@ -72,43 +93,95 @@ char isintable(char *w)
 	return 0;
 }
 
+void init_table()
+{
+	memset(table, 0, tablesize * 32); //set everything to NULL
+}
+
 void insertwords(char *file)
 {
 	FILE *f = fopen(file, "r");
 	char *tmpw = (char *)malloc(180), *sp = tmpw, *insw;
-	int  *wc = (int *)malloc(sizeof(int)), uc = 0;
+	int  *wc = (int *)malloc(sizeof(int)), uc = 0, cmp;
+	unsigned int hk;
 	*wc = 0;
 	memset(tmpw, 0, 120);
-	memset(table, 0, tablesize * 32); //set everything to NULL
 	while (fscanf(f, "%c", sp++) != EOF)
 	{
 		*(sp - 1) = toupper(*(sp - 1));
+		cmp = 0;
 		if (!isalnum(*(sp - 1)) && *(sp - 1) != '\'' && *(sp - 1) != '-')
 		{
 			*(sp - 1) = 0;
 			if ((insw = strcheck(tmpw)) != NULL)
-				if (!isintable(insw))
+				if (!isintable(insw) || getenv("search")[0] == '1')
 				{
 					++uc;
-					if (getenv("probe")[0] == '0')
-						strcpy(table[linearprobe(hash(insw))], insw);
+					if (strcmp(getenv("search"), "0") == 0)
+					{
+						if (getenv("probe")[0] == '0')
+							strcpy(table[linearprobe(hash(insw))], insw);
+						else
+							strcpy(table[quadraticprobe(hash(insw))], insw);
+					}
 					else
-						strcpy(table[quadraticprobe(hash(insw))], insw);
+					{
+						if (getenv("probe")[0] == '0')
+						{
+							//searching using lprobe
+							cmp = 1; //first compare, duh
+							hk = hash(insw);
+							hk = linearprobe(hk, (unsigned int *)&cmp, insw);
+							linearprobeavg += cmp;
+						}
+						else
+						{
+							//quadratic probe search
+							cmp = 1;
+							hk = hash(insw);
+							hk = quadraticprobe(hk, (unsigned int *)&cmp, insw);
+							quadraticprobeavg += cmp + 1; //sort of a hack, less than one
+						}
+					}
 				}
 			++*wc;
-			memset(tmpw, 0, 120);
 			sp = tmpw;
 		}
 	}
-	printf("WC: %d\tUC: %d\n", *wc, uc);
+	fclose(f);
+	//printf("WC: %d\tUC: %d\n", *wc, uc);
+	twc = *wc;
+	tuc = uc;
 }
 
-int main(int argc, char * * argv)
+int main(int argc, char * * argv, char * * envp)
 {
-	int i;
-	tablesize = atoi(getenv("tsize"));
+	linearprobeavg = 0;
+	quadraticprobeavg = 0;
+	listsize = tablesize = atoi(getenv("tsize"));
+	init_table();
+	setenv("search", "0", 1);
 	insertwords(argv[1]);
-	for (i = 0; i < tablesize; i++)
-		printf("%3d : %s\n", i, table[i]);
+	setenv("search", "1", 1);
+	insertwords(argv[1]);
+	printf("%s\tTable size: %s\n", getenv("probe")[0] - 48 ? "Quadratic" : "Linear", getenv("tsize"));
+	printf("Total words in speech: %d\n", twc);
+	printf("Total unique words in speech: %d\n", tuc);
+	printf("Mean search comparisons: %2.2lf\n\n",
+		  (double)((double)(getenv("probe")[0] - 48 ? quadraticprobeavg : linearprobeavg)) / (double)twc);
+	if (tablesize == 611)
+		setenv("tsize", "711", 1);
+	else if (tablesize == 711)
+		setenv("tsize", "1235", 1);
+	else
+	{
+		if (strcmp(getenv("probe"), "0") == 0)
+		{
+			setenv("tsize", "611", 1);
+			setenv("probe", "1", 1);
+		}
+		else return 0;
+	}
+	execve(argv[0], argv, envp); //re-exec
 }
 
---
+++ Project9/Project9/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#make sure you g++ the main.cpp file,
+#then just ./setup.sh ./a.out ./speech.txt
+export tsize=611
+export probe=0
+export search=0
+$1 $2
